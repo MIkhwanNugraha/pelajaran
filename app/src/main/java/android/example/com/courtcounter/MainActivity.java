735d26ce59d9cd3f323f6b4b459c4c6f36cb9c14
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2019-02-24 23:42:48 +0400

    3

--- app/src/main/java/android/example/com/courtcounter/MainActivity.java
+++ app/src/main/java/android/example/com/courtcounter/MainActivity.java
@@ -8,61 +8,65 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    int faultTeamA = 0;
+    int faultTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /**
-     * Displays the given score for Team A.
-     */
-
-    public void displayForTeamA(int score) {
-        TextView scoreView = findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
-    public void displayForTeamB(int score) {
-        TextView scoreView = findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
-    public void threeForA(View view) {
-        scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void twoForA(View view) {
-        scoreTeamA = scoreTeamA + 2;
-        displayForTeamA(scoreTeamA);
+        setTitle("Football Score");
     }
 
     public void oneForA(View view) {
         scoreTeamA = scoreTeamA + 1;
-        displayForTeamA(scoreTeamA);
+        displayScoreForTeamA(scoreTeamA);
     }
 
-    public void threeForB(View view) {
-        scoreTeamB = scoreTeamB + 3;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void twoForB(View view) {
-        scoreTeamB = scoreTeamB + 2;
-        displayForTeamB(scoreTeamB);
+    public void faultForA(View view) {
+        faultTeamA = faultTeamA + 1;
+        displayFaultForTeamA(faultTeamA);
     }
 
     public void oneForB(View view) {
         scoreTeamB = scoreTeamB + 1;
-        displayForTeamB(scoreTeamB);
+        displayScoreForTeamB(scoreTeamB);
+    }
+
+    public void faultForB(View view) {
+        faultTeamB = faultTeamB + 1;
+        displayFaultForTeamB(faultTeamB);
     }
 
     public void putZero(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        faultTeamA = 0;
+        faultTeamB = 0;
+        displayScoreForTeamA(scoreTeamA);
+        displayScoreForTeamB(scoreTeamB);
+        displayFaultForTeamA(faultTeamA);
+        displayFaultForTeamB(faultTeamB);
+    }
+
+    public void displayScoreForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayFaultForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_fault);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayScoreForTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayFaultForTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_fault);
+        scoreView.setText(String.valueOf(score));
     }
 }
